f78106f3f45dd7ac3d1d142220ed29e20354338f
bhavi-khairnar317 <bhavikakhairnar317@gmail.com>
2024-09-16 08:19:34 +0530

    API code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+//import { Console } from 'console';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'API_integrate_project';
+  public data:any;
+ constructor(private http:HttpClient){}
+  
+  ngOnInit(): void {
+    this.fetchDetails();
+  }
+  public fetchDetails(){
+   this.http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(
+    (resp:any) => { 
+      //console.log(resp); 
+       this.data = resp; 
+    }
+   )
+  }
 }
